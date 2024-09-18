646e62c77c9ae4697692b4a417959a7228ddbcd9
Igor Starchenko <garganss@yandex.ru>
2024-09-18 09:27:51 +0300

    Повышена плавность работы

--- motors_control.py
+++ motors_control.py
@@ -5,8 +5,8 @@ position = 66.1233251
 #скорость(км/ч)
 velocity = 10;
 #нормировочный коэффициент скорости
-velocity_delimiter = 0.0001;
+velocity_delimiter = 0.00005;
 while True:
-    time.sleep(1)
+    time.sleep(0.5)
     print ("we fly at: ", position)
     position += velocity * velocity_delimiter
